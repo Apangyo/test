c2bbf659a84f367fe69109cb24afd85695ba9c29
apang <945441965@qq.com>
2019-12-18 17:32:38 +0800

    first commit

---
+++ src/main/java/com/apang/test/Test.java
@@ -0,0 +1,14 @@
+package com.apang.test;
+
+/**
+ * @Auther: apang
+ * @Date:2019/12/18
+ * @Description:com.apang.test
+ * @version:1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        System.out.println("first test");
+    }
+}
